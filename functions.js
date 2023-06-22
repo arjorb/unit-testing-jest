f4782f92036a03349214c952ad9a44ab2ba9ce26
John U <arjorb@gmail.com>
2023-06-22 18:02:19 +0200

    test the createUser function which return object

--- functions.js
+++ functions.js
@@ -1,5 +1,10 @@
 const functions = {
     add: (num1,num2) => num1+num2 ,
-    isNull: () => null
+    isNull: () => null,
+    createUser: ()=>{
+       const user = {firstname:'John'};
+        user['lastname']='Uwimana';
+        return user;
+    }
 }
 module.exports = functions;
--- functions.test.js
+++ functions.test.js
@@ -8,3 +8,9 @@ test("check if the fuction is null",()=>{
     expect(functions.isNull()).toBeNull();
 })
 
+test("check if the user is John Uwimana Object",()=>{
+    expect(functions.createUser()).toEqual({
+        firstname:'John', 
+        lastname:'Uwimana'
+    });
+})
