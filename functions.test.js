47eb5344109160d847e4153392742c158b393f22
John U <arjorb@gmail.com>
2023-06-22 17:42:40 +0200

    testing if the function is null

--- functions.test.js
+++ functions.test.js
@@ -1,6 +1,10 @@
 const functions = require('./functions');
 
-
 test("adding add method",()=>{
     expect(functions.add(2,2)).toBe(4)
 })
+
+test("check if the fuction is null",()=>{
+    expect(functions.isNull()).toBeNull();
+})
+
