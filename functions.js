86ef81ed269e07eb93fda783758062d434d4802e
John U <arjorb@gmail.com>
2023-06-26 16:35:27 +0200

    working with function fetchUser

--- functions.js
+++ functions.js
@@ -1,3 +1,5 @@
+const { default: axios } = require("axios");
+
 const functions = {
     add: (num1,num2) => num1+num2 ,
     isNull: () => null,
@@ -5,6 +7,9 @@ const functions = {
        const user = {firstname:'John'};
         user['lastname']='Uwimana';
         return user;
-    }
+    },
+    fectUser: ()=> axios.get('https://jsonplaceholder.typicode.com/users/1').
+    then(res => res.data).
+    catch(error => 'error')
 }
 module.exports = functions;
